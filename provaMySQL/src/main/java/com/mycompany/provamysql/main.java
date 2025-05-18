4a1ee140cf13b833aa05e977de9582b97feeac91
Evellynsnt <159397519+Evellynsnt@users.noreply.github.com>
2025-05-18 20:08:27 -0300

    Add files via upload

---
+++ provaMySQL/src/main/java/com/mycompany/provamysql/main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.provamysql;
+
+/**
+ *
+ * @author evellynsantospinto
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
