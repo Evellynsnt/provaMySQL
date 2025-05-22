b58d8f6beecdfe996d3aac4255eac2f427c276b1
Evellynsnt <159397519+Evellynsnt@users.noreply.github.com>
2025-05-22 10:16:39 -0300

    Update ConsultaController.java

--- provaMySQL-main/provaMySQL/src/main/java/controller/ConsultaController.java
+++ provaMySQL-main/provaMySQL/src/main/java/controller/ConsultaController.java
@@ -5,6 +5,7 @@ import dao.ConsultaDAO;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import model.Consulta;
+import java.util.ArrayList;
 
 /**
  *
@@ -16,6 +17,19 @@ public class ConsultaController {
     public void cadastrar(int idPaciente, int idMedico, LocalDate data, LocalTime hora, String observacao) {
         Consulta c = new Consulta(idPaciente, idMedico, data, hora, observacao);
         dao.inserir(c);
+    }
+
+       public ArrayList<Consulta> listar() {
+        return dao.listar();
+    }
+    
+    public void atualizar(int idConsulta, int idPaciente, int idMedico, String nomeMedico, LocalDate data, LocalTime hora , String observacao) {
+        Consulta c = new Consulta(idConsulta, idMedico, idPaciente, data, hora, observacao);
+        dao.atualizar(c);
+    }
+    
+    public void remover(int idMedico) {
+        dao.remover(idMedico);
     }
     
    
