73e280ca129c4aea541e66ae75980e22bb0ad19e
Evellynsnt <159397519+Evellynsnt@users.noreply.github.com>
2025-05-22 09:01:37 -0300

    Update ConexaoMySQL.java

--- provaMySQL-main/provaMySQL/src/main/java/ConexaoMySQL/ConexaoMySQL.java
+++ provaMySQL-main/provaMySQL/src/main/java/ConexaoMySQL/ConexaoMySQL.java
@@ -10,7 +10,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoMySQL {
-    private static final String URL = "jdbc:mysql://localhost:3306/provaMySQL";
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbl";
     private static final String USER = "root";
     private static final String PASSWORD = "";
     
